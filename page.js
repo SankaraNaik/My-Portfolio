1db2f3c996eb77645ca880b9c1eb13d0317e85ff
SankaraNaik <sankaranaik777@gmail.com>
2023-10-02 22:47:26 +0530

    made responsive

--- page.js
+++ page.js
@@ -1,8 +1,7 @@
 function toggleMenu() {
-
-const menu=document.querySelector(".menu-link");
-const icon=document.querySelector(".humburger-icon");
-menu.classList.toggle("open");
-icon.classList.toggle("open");
-}
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
 
